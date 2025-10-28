afd669b060298ac68019dca067e4f654eb1da3c9
Alyssa Magararo <magararoalyssa_bsit@plmun.edu.ph>
2025-10-28 15:24:25 +0800

    Update MainActivity.kt

--- Final_Laboratories/Final_Lab#1/MainActivity.kt
+++ Final_Laboratories/Final_Lab#1/MainActivity.kt
@@ -1,1 +1,14 @@
+//Magararo, Alyssa M.
+//BSIT-3B
+//MainActivity
+package com.example.mainactivity
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
