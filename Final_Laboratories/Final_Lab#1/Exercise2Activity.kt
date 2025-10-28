84896af4251d7d82dfcce6335e0d4e2892f8401d
Alyssa Magararo <magararoalyssa_bsit@plmun.edu.ph>
2025-10-28 15:25:44 +0800

    Create Exercise2Activity.kt

---
+++ Final_Laboratories/Final_Lab#1/Exercise2Activity.kt
@@ -0,0 +1,15 @@
+//Magararo, Alyssa M.
+//BSIT-3B
+//Exercise #2
+
+package com.example.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercise2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise2)
+    }
+}
